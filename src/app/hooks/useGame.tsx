c6c5dfc81b1d5edad64ef01cc6220a38da78b1c5
Kilian Cabrera <kilianbboy@gmail.com>
2023-06-18 20:42:23 +0200

    Reorder usegame functions

--- src/app/hooks/useGame.tsx
+++ src/app/hooks/useGame.tsx
@@ -1,16 +1,67 @@
-import { ChangeEvent, SetStateAction, useEffect, useState } from "react";
+import {
+  ChangeEvent,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import { faker } from "@faker-js/faker";
+import { ICurrentPosition } from "../types";
 
 export const useGame = () => {
   const [words, setWords] = useState<string[][]>([]);
-  const [currentPosition, setCurrentPosition] = useState<{
-    x: number;
-    y: number;
-  }>({ x: 0, y: 0 });
+  const [currentPosition, setCurrentPosition] = useState<ICurrentPosition>({
+    x: 0,
+    y: 0,
+  });
   const [inputValue, setInputValue] = useState<string>("");
   const [ended, setEnded] = useState<boolean>(false);
+  const [started, setStarted] = useState<boolean>(false);
   const [intervalTimer, setIntervalTimer] = useState<NodeJS.Timer | null>(null);
   const [time, setTime] = useState<number>(0);
+
+  const startGame = () => {
+    setStarted(true);
+    resetGame();
+    let words: string[][] = [];
+
+    for (let i = 0; i < 5; i++) {
+      words[i] = [];
+
+      for (let j = 0; j < 5; j++) {
+        words[i].push(faker.word.noun());
+      }
+    }
+
+    setWords(words);
+
+    const startTime = Date.now();
+
+    const interval = setInterval(() => {
+      const elapsedTime = Date.now() - startTime;
+      setTime(elapsedTime);
+    }, 10);
+
+    setIntervalTimer(interval);
+  };
+
+  const resetGame = () => {
+    clearInterval(intervalTimer!);
+    setEnded(false);
+    setCurrentPosition({
+      x: 0,
+      y: 0,
+    });
+  };
+
+  const endGame = () => {
+    setEnded(true);
+    clearInterval(intervalTimer!);
+  };
+
+  const formatTime: string = useMemo(() => {
+    return new Date(time).toISOString().slice(14, -1);
+  }, [time]);
 
   const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
     const typedWord = e.target.value;
@@ -42,49 +93,6 @@ export const useGame = () => {
     }
   };
 
-  const endGame = () => {
-    setEnded(true);
-    clearInterval(intervalTimer!);
-  };
-
-  const resetGame = () => {
-    clearInterval(intervalTimer!);
-    setEnded(false);
-    setCurrentPosition({
-      x: 0,
-      y: 0,
-    });
-  };
-
-  const startGame = () => {
-    document.getElementById("Input")?.focus();
-    resetGame();
-    let words: string[][] = [];
-
-    for (let i = 0; i < 5; i++) {
-      words[i] = [];
-
-      for (let j = 0; j < 5; j++) {
-        words[i].push(faker.word.noun());
-      }
-    }
-
-    setWords(words);
-
-    const startTime = Date.now();
-
-    const interval = setInterval(() => {
-      const elapsedTime = Date.now() - startTime;
-      setTime(elapsedTime);
-    }, 10);
-
-    setIntervalTimer(interval);
-  };
-
-  const formatTime = () => {
-    return new Date(time).toISOString().slice(14, -1);
-  };
-
   return {
     words,
     currentPosition,
@@ -94,5 +102,6 @@ export const useGame = () => {
     inputValue,
     time,
     formatTime,
+    started,
   };
 };
